b45a3906f2cde696c027c951698eecfb3623ef98
Jesus Luciani <jesusluciani833@hotmail.com>
2020-09-13 10:02:27 -0400

    docs agragado

---
+++ docs/precache-manifest.cc858a1231076a1b372ea6f1671b141b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d1d3e79fc180d22cbb98a80142f944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86cf3b1b555264db594f",
+    "url": "/static/css/main.5f707190.chunk.css"
+  },
+  {
+    "revision": "1a05bea31f23bc87781d",
+    "url": "/static/js/2.d9079585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9079585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86cf3b1b555264db594f",
+    "url": "/static/js/main.1b2e36fe.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
